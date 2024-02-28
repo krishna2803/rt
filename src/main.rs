1f825510f7827ffb3846568aafa0e6229a5542e1
krishna2803 <kpandey81930@gmail.com>
2024-02-28 17:12:36 +0530

    did stuff

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
-pub mod maths;
-pub mod hittable;
+pub mod hittables;
 pub mod image;
+pub mod maths;
+pub mod objects;
 
-use std::time::Duration;
-
-use pbr::ProgressBar;
-
-use maths::*;
+use hittables::*;
 use image::*;
+use maths::*;
+use objects::*;
 
 fn write_color(c: &color, data: &mut Vec<u8>) {
     unsafe {
@@ -18,27 +17,10 @@ fn write_color(c: &color, data: &mut Vec<u8>) {
     }
 }
 
-fn hit_sphere(center: point3, radius: f64, r: &ray) -> f64 {
-    let oc = r.origin() - center;
-    let a = r.direction().length_squared();
-    let half_b = vec3::dot(oc, r.direction());
-    let c = oc.length_squared() - radius*radius;
-    let discriminant = half_b * half_b - a*c;
-
-    if discriminant < 0.0 {
-        -1.0
-    } else {
-        (-half_b - discriminant.sqrt()) / a
-    }
-}
-
-fn ray_color(r: &ray) -> color {
-    let center = point3::new(0.0, 0.0, -1.0);
-    let radius = 0.5;
-    let t = hit_sphere(center, radius, r);
-    if t > 0.0 {
-        let n = (r.at(t) - center).normalized();
-        color::new(n.x()+1.0, n.y()+1.0, n.z()+1.0) * 0.5
+fn ray_color(r: &ray, world: &hittable_list) -> color {
+    let mut rec = hit_record::null();
+    if world.clone().hit(r, 0.0, f64::INFINITY, &mut rec) {
+        (rec.normal + color::from_scalar(1.0)) * 0.5
     } else {
         let unit_direction = r.direction().normalized();
         let a = 0.5 * (unit_direction.y() + 1.0);
@@ -49,14 +31,26 @@ fn ray_color(r: &ray) -> color {
 }
 
 fn main() {
+    // Image
     let aspect_ratio: f64 = 16.0 / 10.0;
     let image_width: u32 = 1000;
     let image_height: u32 = ((image_width as f64 / aspect_ratio as f64) as u32).max(1);
 
-    // camera
+    // World
+    let mut world: hittable_list = hittable_list::new();
+    world.add(hittable_obj::sphere(sphere::new(
+        point3::new(0.0, 0.0, -1.0),
+        0.5,
+    )));
+    world.add(hittable_obj::sphere(sphere::new(
+        point3::new(0.0, -100.5, -1.0),
+        100.0,
+    )));
+
+    // Camera
     let focal_length: f64 = 1.0;
     let viewport_height: f64 = 2.0;
-    let viewport_width: f64 =  viewport_height * (image_width as f64 / image_height as f64);
+    let viewport_width: f64 = viewport_height * (image_width as f64 / image_height as f64);
     let camera_center: vec3 = point3::new(0.0, 0.0, 0.0);
 
     // Calculate the vectors across the horizontal and down the vertical viewport edges.
@@ -68,25 +62,26 @@ fn main() {
     let pixel_delta_v = viewport_v / (image_height as f64);
 
     // Calculate the location of the upper left pixel.
-    let viewport_upper_left = camera_center - vec3::new(0.0, 0.0, focal_length) - viewport_u * 0.5 - viewport_v * 0.5;
+    let viewport_upper_left =
+        camera_center - vec3::new(0.0, 0.0, focal_length) - viewport_u * 0.5 - viewport_v * 0.5;
     let pixel00_loc = viewport_upper_left + (pixel_delta_u + pixel_delta_v) * 0.5;
 
     let mut image_data: Vec<u8> = Vec::new();
 
-    let mut pb = ProgressBar::new(image_height as u64);
+    let mut pb = pbr::ProgressBar::new(image_height as u64);
     pb.show_speed = false;
     pb.show_message = true;
     pb.message("Scanline ");
     pb.format("[=> ]");
 
-
     for j in 0..image_height {
         for i in 0..image_width {
-            let pixel_center = pixel00_loc + (pixel_delta_u * i as f64) + (pixel_delta_v * j as f64);
+            let pixel_center =
+                pixel00_loc + (pixel_delta_u * i as f64) + (pixel_delta_v * j as f64);
             let ray_direction = pixel_center - camera_center;
             let r = ray::new(camera_center, ray_direction);
-            
-            let pixel_color = ray_color(&r);
+
+            let pixel_color = ray_color(&r, &world);
             write_color(&pixel_color, &mut image_data);
         }
         pb.inc();
--- src/objects.rs
+++ src/objects.rs
@@ -1,38 +1,15 @@
 use crate::maths::*;
+use crate::hittables::*;
 
 #[derive(Clone, Copy)]
 #[allow(non_camel_case_types)]
-struct hit_record {
-    p: point3,
-    normal: vec3,
-    t: f64,
-    front_face: bool,
-}
-
-impl hit_record {
-    pub fn set_face_normal(mut self, r: &ray, outward_normal: vec3) {
-        self.front_face = vec3::dot(r.direction(), outward_normal) < 0.0;
-        self.normal = if self.front_face {
-            outward_normal
-        } else {
-            -outward_normal
-        };
-    }
-}
-
-#[allow(non_camel_case_types)]
-trait hittable {
-    fn hit(self, r: &ray, ray_tmin: f64, ray_tmax: f64, rec: &mut hit_record) -> bool;
-}
-
-#[allow(non_camel_case_types)]
-struct sphere {
+pub struct sphere {
     center: point3,
     radius: f64,
 }
 
 impl sphere {
-    fn new(center: point3, radius: f64) -> sphere {
+    pub fn new(center: point3, radius: f64) -> sphere {
         sphere { center, radius }
     }
 }
